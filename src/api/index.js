a607ca8dd60124f2a9b8eea3d76468b307345e4a
Ar11rA <aritra119.26@gmail.com>
2018-07-11 13:54:36 +0530

    Used config in api class

--- src/api/index.js
+++ src/api/index.js
@@ -11,21 +11,21 @@ const headers = {
 module.exports = {
   create: (index, type, data) => {
     return axios
-      .post(`http://localhost:9200/${index}/${type}?pretty`, data, headers);
+      .post(`${serverUrl}/${index}/${type}?pretty`, data, headers);
   },
   lazyDelete: (index) => {
     return axios
-      .delete(`http://localhost:9200/${index}`);
+      .delete(`${serverUrl}/${index}`);
   },
   eagerDelete: (params) => {
     return axios
-      .post(`http://localhost:9200/${index}/_delete_by_query`, params, headers);
+      .post(`${serverUrl}/${index}/_delete_by_query`, params, headers);
   },
   eagerSearch: (index, params) => {
-    return axios.post(`http://localhost:9200/${index}/_search`, params, headers);
+    return axios.post(`${serverUrl}/${index}/_search`, params, headers);
   },
   lazySearch: (index, queryTerm) => {
-    return axios.post(`http://localhost:9200/${index}/_search`, {
+    return axios.post(`${serverUrl}/${index}/_search`, {
       'query' : {
         'term' : queryTerm
       }
